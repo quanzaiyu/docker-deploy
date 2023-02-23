69797138f440da672ae7f473dcac317c0272b52d
quanzaiyu <731734107@qq.com>
2023-02-23 14:09:15 +0800

    nacos

--- docker/hoppscotch.bat
+++ docker/hoppscotch.bat
@@ -1,1 +1,1 @@
-docker run --rm --name hoppscotch -p 3000:3000 hoppscotch/hoppscotch:latest
+docker run -d --name hoppscotch -p 3000:3000 hoppscotch/hoppscotch:latest
---
+++ docker/nacos.bat
@@ -0,0 +1,1 @@
+docker run --name nacos-standalone -e MODE=standalone -d -p 8848:8848 -p 9848:9848 -p 9849:9849 nacos/nacos-server:2.0.0
